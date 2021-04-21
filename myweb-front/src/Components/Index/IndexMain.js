39f8067827c690a41627e0d9aeb2e1e2282857ea
Positive-Conative <kyj970513@naver.com>
2021-04-21 13:20:40 +0000

    Create&Edit : front

---
+++ myweb-front/src/Components/Index/IndexMain.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import Button from '@material-ui/core/Button';
+
+class IndexMain extends Component{
+  render(){
+    return(
+        <div>
+            <Button variant="contained" color="primary">
+                Primary
+              </Button>
+        </div>
+    )
+  }
+}
+
+export default IndexMain;
